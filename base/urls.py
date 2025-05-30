d57390a463d2071c4fac7196130ed812205c84bb
damil27 <dhamilare27@gmail.com>
2025-05-29 22:06:05 -0700

    Authentication implementation

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.loginPage, name='login'),  # Login page
     path('', views.home, name='home'),  # Home page
     path("room", views.room, name="room"),  # Room page
     path("details/<int:pk>", views.details, name="room_details"),  # Room page with primary key
     path("create_room", views.create_room, name="create_room"),  # Create room page
     path("update_room/<int:pk>", views.update_room, name="update_room"),  # Update room page
-    # path("delete_room/<int:pk>", views.delete_room, name="delete_room"),  # Delete room page
+    path("delete_room/<int:pk>", views.delete_room, name="delete_room"),  # Delete room page
 ]
--- base/views.py
+++ base/views.py
@@ -1,16 +1,48 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User     
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 
 def home(request):
         return render(request,"base/home.html")
 
+def loginPage(request):
+        if request.method == 'POST':
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                try:
+                        user = User.objects.get(username=username)
+                except:
+                        messages.error(request, "User does not exist")
+                        # return render(request, "base/login_register.html") 
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                        login(request, user)
+                        return redirect('room')
+                else:
+                        messages.error(request, "Username or password is incorrect")
+                        # return render(request, "base/login_register.html")
+
+        contenxt = {}
+        return render(request, "base/login_register.html", contenxt)
+
 def room(request):
-        rooms = Room.objects.all()
-        return render(request,"base/room.html",{'rooms': rooms})
+        q = request.GET.get('q') if request.GET.get('q') != None else ''
+        rooms = Room.objects.filter(
+                Q(topic__name__icontains=q)| 
+                Q(name__icontains=q) |
+                Q(description__icontains=q)
+                )
+        topics =  Topic.objects.all()
+        rooms_count = rooms.count()
+        context =  {'rooms': rooms, 'topics': topics, 'rooms_count': rooms_count}
+        return render(request,"base/room.html",context)
 
 def details(request, pk):
         room = Room.objects.get(id=pk) 
@@ -42,9 +74,16 @@ def update_room(request, pk):
                         form.save()
                         return redirect('room')
         # The above code defines a view for updating a room in a Django application.
-                        
-
-
 
         context = {'form': form}
         return render(request, "base/create_room.html", context)
+
+def delete_room(request, pk): 
+        room  =  Room.objects.get(id=pk)
+        form = RoomForm(instance=room)
+        if request.method == 'POST':
+                room.delete()
+                return redirect('room')
+        
+        return render(request, "base/delete.html", {'obj':room})
+# The above code defines a view for deleting a room in a Django application.
